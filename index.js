92100cc328b38b64fac828c18d326fe491ea0899
lijink14@gmail.com <lijink14@gmail.com>
2019-10-23 16:44:03 +0530

    updated code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('creating lambda from github'),
+        body: JSON.stringify('creating lambda from github latest'),
     };
     return response;
 };
