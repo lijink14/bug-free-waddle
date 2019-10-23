2bb34f10aa3034e5981201103b0af35dd3cf09bd
lijink14 <53892111+lijink14@users.noreply.github.com>
2019-10-23 17:17:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('creating lambda from github latest'),
+        body: JSON.stringify('creating lambda from github latest pipeline'),
     };
     return response;
 };
