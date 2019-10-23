9412009370da494ae665c0f1fac4b949a1833d48
lijink14@gmail.com <lijink14@gmail.com>
2019-10-23 15:31:35 +0530

    the updated code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Github sync test'),
+        body: JSON.stringify('Github sync test1'),
     };
     return response;
 };
